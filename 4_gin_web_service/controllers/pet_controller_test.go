3d749bca8bca24247b7e041a6d4b8054ef754611
Kaushik Neelichetty <kaushikn@thoughtworks.com>
2019-08-02 15:23:50 +0530

    add controller tests for CreatePet

--- 4_gin_web_service/controllers/pet_controller_test.go
+++ 4_gin_web_service/controllers/pet_controller_test.go
@@ -1,58 +1,91 @@
 package controllers
 
 import (
-	"4_gin_web_service/repositories"
-	"4_gin_web_service/services"
+	"4_gin_web_service/generated_mocks"
+	"4_gin_web_service/models"
 	"bytes"
+	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func badResponseValidator(recorder *httptest.ResponseRecorder, t *testing.T) {
-	assert.Equal(t, 400, recorder.Code)
+type PetControllerTestSuite struct {
+	suite.Suite
+	mockCtrl       *gomock.Controller
+	recorder       *httptest.ResponseRecorder
+	context        *gin.Context
+	mockPetService *generated_mocks.MockPetService
+	petController  PetController
 }
 
-func Test_petController_CreatePet(t *testing.T) {
-	type args struct {
-		requestBody       string
-		responseValidator func(recorder *httptest.ResponseRecorder, t *testing.T)
+func TestPetControllerTestSuite(t *testing.T) {
+	suite.Run(t, new(PetControllerTestSuite))
+}
+
+func (suite *PetControllerTestSuite) SetupTest() {
+	suite.mockCtrl = gomock.NewController(suite.T())
+	suite.recorder = httptest.NewRecorder()
+	suite.context, _ = gin.CreateTestContext(suite.recorder)
+	suite.mockPetService = generated_mocks.NewMockPetService(suite.mockCtrl)
+	suite.petController = NewPetController(suite.mockPetService)
+}
+
+func (suite *PetControllerTestSuite) TearDownTest() {
+	suite.mockCtrl.Finish()
+}
+
+func (suite *PetControllerTestSuite) TestCreatePet_WhenFailureInParsingRequestBody() {
+	requestBody := `{"someKey" : "someValue"}`
+	suite.context.Request, _ = http.NewRequest("POST", "/", bytes.NewBufferString(requestBody))
+
+	suite.petController.CreatePet(suite.context)
+
+	suite.Require().Equal(http.StatusBadRequest, suite.recorder.Code)
+}
+
+func (suite *PetControllerTestSuite) TestCreatePet_WhenFailureInRequestBodyValidation() {
+	requestBody := `{"id":123123,"name":"doggie","photoUrls":["string"],"status":"unavailable"}`
+	suite.context.Request, _ = http.NewRequest("POST", "/", bytes.NewBufferString(requestBody))
+
+	suite.petController.CreatePet(suite.context)
+
+	suite.Require().Equal(http.StatusMethodNotAllowed, suite.recorder.Code)
+}
+
+func (suite *PetControllerTestSuite) TestCreatePet_WhenFailureInService() {
+	requestBody := `{"id":123123,"name":"doggie","photoUrls":["string"],"status":"available"}`
+	suite.context.Request, _ = http.NewRequest("POST", "/", bytes.NewBufferString(requestBody))
+	pet := models.Pet{
+		Id:        123123,
+		Name:      "doggie",
+		PhotoUrls: []string{"string"},
+		Status:    "available",
 	}
-	tests := []struct {
-		name string
-		args args
-	}{
-		{
-			name: "fail when ID is not present",
-			args: args{
-				requestBody:       `{"name":"Tom","status":"available","photoUrls":[]}`,
-				responseValidator: badResponseValidator,
-			},
-		},
-		{
-			name: "fail when ID is an integer",
-			args: args{
-				requestBody: `{"id":"123","name":"Tom","status":"available","photoUrls":[]}`,
-				responseValidator: func(recorder *httptest.ResponseRecorder, t *testing.T) {
-					assert.Equal(t, 400, recorder.Code)
-				},
-			},
-		},
+
+	suite.mockPetService.EXPECT().CreatePet(pet).Return(errors.New("some error")).Times(1)
+
+	suite.petController.CreatePet(suite.context)
+
+	suite.Require().Equal(http.StatusConflict, suite.recorder.Code)
+}
+
+func (suite *PetControllerTestSuite) TestCreatePet() {
+	requestBody := `{"id":123123,"name":"doggie","photoUrls":["string"],"status":"available"}`
+	suite.context.Request, _ = http.NewRequest("POST", "/", bytes.NewBufferString(requestBody))
+	pet := models.Pet{
+		Id:        123123,
+		Name:      "doggie",
+		PhotoUrls: []string{"string"},
+		Status:    "available",
 	}
-	for _, tt := range tests {
-		recorder := httptest.NewRecorder()
-		context, _ := gin.CreateTestContext(recorder)
-		context.Request, _ = http.NewRequest("POST", "/", bytes.NewBufferString(tt.args.requestBody))
-		t.Run(tt.name, func(t *testing.T) {
-			gomock.NewController(t)
-			repository := repositories.NewPetRepository()
-			service := services.NewPetService(repository)
-			controller := NewPetController(service)
-			controller.CreatePet(context)
-			tt.args.responseValidator(recorder, t)
-		})
-	}
+
+	suite.mockPetService.EXPECT().CreatePet(pet).Return(nil).Times(1)
+
+	suite.petController.CreatePet(suite.context)
+
+	suite.Require().Equal(http.StatusCreated, suite.recorder.Code)
 }
