35457196b194798c609540bad768dd7076d21ebd
Kaushik Neelichetty <kaushikn@thoughtworks.com>
2019-08-02 15:51:46 +0530

    add controller tests for GetPEt

--- 4_gin_web_service/controllers/pet_controller_test.go
+++ 4_gin_web_service/controllers/pet_controller_test.go
@@ -89,3 +89,58 @@ func (suite *PetControllerTestSuite) TestCreatePet() {
 
 	suite.Require().Equal(http.StatusCreated, suite.recorder.Code)
 }
+
+func (suite *PetControllerTestSuite) TestGetPet_WhenFailureInMissingPetID() {
+	suite.context.Params = gin.Params{}
+
+	suite.petController.GetPet(suite.context)
+
+	suite.Require().Equal(http.StatusBadRequest, suite.recorder.Code)
+}
+
+func (suite *PetControllerTestSuite) TestGetPet_WhenFailureInParsingPetID() {
+	suite.context.Params = gin.Params{gin.Param{
+		Key:   "petID",
+		Value: "someInvalidPetID",
+	}}
+
+	suite.petController.GetPet(suite.context)
+
+	suite.Require().Equal(http.StatusMethodNotAllowed, suite.recorder.Code)
+}
+
+func (suite *PetControllerTestSuite) TestGetPet_WhenServiceFails() {
+	suite.context.Params = gin.Params{gin.Param{
+		Key:   "petID",
+		Value: "123456",
+	}}
+	petID := 123456
+
+	suite.mockPetService.EXPECT().GetPet(petID).Return(models.Pet{}, errors.New("some error")).Times(1)
+
+	suite.petController.GetPet(suite.context)
+
+	suite.Require().Equal(http.StatusNotFound, suite.recorder.Code)
+}
+
+func (suite *PetControllerTestSuite) TestGetPet() {
+	suite.context.Params = gin.Params{gin.Param{
+		Key:   "petID",
+		Value: "123456",
+	}}
+	petID := 123456
+	pet := models.Pet{
+		Id:        123123,
+		Name:      "doggie",
+		PhotoUrls: []string{"string"},
+		Status:    "available",
+	}
+	expectedResponse := `{"id":123123,"name":"doggie","photoUrls":["string"],"status":"available"}`
+
+	suite.mockPetService.EXPECT().GetPet(petID).Return(pet, nil).Times(1)
+
+	suite.petController.GetPet(suite.context)
+
+	suite.Require().Equal(http.StatusOK, suite.recorder.Code)
+	suite.Require().Equal(expectedResponse, suite.recorder.Body.String())
+}
